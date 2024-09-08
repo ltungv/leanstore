4296b2289b96c06b5465d65bab3a8f2e518d49a0
leonlolly <leon@wenderoth.de>
2024-09-08 21:58:50 +0200

    add bookkeeper_metadata_uri flag

--- leanstore_runner.py
+++ leanstore_runner.py
@@ -23,18 +23,30 @@ def run_cli_command(command, output_file):
 if __name__ == "__main__":
     output_file = None
     tpcc_directory = None
+    bookkeeper_metadata_uri = None
 
-    if len(sys.argv) == 3:
+    if len(sys.argv) == 4:
         output_file = sys.argv[1]
         tpcc_path = sys.argv[2]
+        bookkeeper_metadata_uri = sys.argv[3]
+ 
 
     if output_file is None or len(output_file) < 1:
-        print("Usage: python3 leanstore_runner.py <output file names> <tpcc_directory> ")
+        print("Usage: python3 leanstore_runner.py <output file names> <tpcc_directory> <bookkeeper_metadata_uri>")
+        sys.exit(1)
+
+    if tpcc_path is None:
+        print("Usage: python3 leanstore_runner.py <output file names> <tpcc_directory> <bookkeeper_metadata_uri>")
+        sys.exit(1)
+    
+    if bookkeeper_metadata_uri is None:
+        print("Usage: python3 leanstore_runner.py <output file names> <tpcc_directory> <bookkeeper_metadata_uri>")
         sys.exit(1)
 
 
 
     os.chdir(Path(tpcc_path))
     
-    command = f"./tpcc --tpcc_warehouse_count=1 --notpcc_warehouse_affinity --worker_threads=4 --pp_threads=16 --free_pct=1 --contention_split --xmerge --warmup_for_seconds=1 --run_for_seconds=2 --isolation_level=si --bookkeeper_jar_directories=../../bookkeeper-wal/target:../../bookkeeper-wal/target/maven-dependencies  --wal_pwrite=true --profile_latency=true --csv_path={output_file} --wal_variant=0"
+    command = f"./tpcc --tpcc_warehouse_count=1 --notpcc_warehouse_affinity --worker_threads=4 --pp_threads=16 --free_pct=1 --contention_split --xmerge --warmup_for_seconds=1 --run_for_seconds=2 --isolation_level=si --bookkeeper_jar_directories=../../bookkeeper-wal/target:../../bookkeeper-wal/target/maven-dependencies  --wal_pwrite=true --profile_latency=true --csv_path={output_file} --wal_variant=0 --bookkeeper_metadata_uri={bookkeeper_metadata_uri}"
     run_cli_command(command, output_file)
+
